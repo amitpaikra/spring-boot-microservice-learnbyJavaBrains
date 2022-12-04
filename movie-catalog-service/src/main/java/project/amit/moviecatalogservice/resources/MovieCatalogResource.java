fa45bbc2ec8279c522c6de6b740a419dcda289d5
Amit Paikra <paikramit1999@gmail.com>
2022-12-04 15:07:42 +0530

    init Commit

---
+++ movie-catalog-service/src/main/java/project/amit/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,10 @@
+package project.amit.moviecatalogservice.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieCatalogResource {
+	
+//	public List<Catalog>
+	
+}
---
+++ movie-info-service/src/test/java/project/amit/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.amit.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
